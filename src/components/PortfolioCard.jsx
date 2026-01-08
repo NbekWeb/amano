e1620df6e830bad6e9fbe8eec2cc1e7526763449
NbekWeb <nbekdev@gmail.com>
2026-01-08 19:00:28 +0500

    fix portfolio

--- src/components/PortfolioCard.jsx
+++ src/components/PortfolioCard.jsx
@@ -7,18 +7,20 @@ const PortfolioCard = ({ title, value, subtitle, icon, showTrend }) => {
   const IconComponent = icon === "coin" ? CoinIcon : IncomeIcon;
 
   return (
-    <div className="dashboard-card  p-6 relative h-fit w-full">
+    <div className="dashboard-card  p-6 relative h-fit w-full flex gap-3">
       <div className="stat-card-blur  w-40   h-12 absolute top-10 right-10 flex"></div>
       <div className="flex items-start justify-between mb-4">
-        <LiquidGlassButton>
+        <LiquidGlassButton width={44} height={44} radius="full">
           <IconComponent />
         </LiquidGlassButton>
       </div>
-      <p className="text-gray-400 text-sm mb-2 ">{title}</p>
-      <p className="text-white  text-2xl font-bold">{value}</p>
-      <div className="flex items-center gap-1.5">
-        <img src={highIcon} alt="trend up" className="w-3 h-3" />
-        <p className="text-white/70 text-xs">{subtitle}</p>
+      <div>
+        <p className="text-gray-400 text-sm mb-2 ">{title}</p>
+        <p className="text-white  text-2xl font-bold">{value}</p>
+        <div className="flex items-center gap-1.5">
+          <img src={highIcon} alt="trend up" className="w-3 h-3" />
+          <p className="text-white/70 text-xs">{subtitle}</p>
+        </div>
       </div>
     </div>
   );
